91b6f62dc0702152b02892fee1874a9d21ed8e8d
Dominik Bittner <DoBi-tyndur@gmx.net>
2015-09-07 18:18:05 +0200

    Figur Klasse hinzugefügt

---
+++ src/MenschAergerDichNicht/Figur.java
@@ -0,0 +1,5 @@
+package MenschAergerDichNicht;
+
+public class Figur {
+
+}
--- src/MenschAergerDichNicht/Start.java
+++ src/MenschAergerDichNicht/Start.java
@@ -1,11 +1,8 @@
 package MenschAergerDichNicht;
 
 public class Start {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		
 	}
-
 }
