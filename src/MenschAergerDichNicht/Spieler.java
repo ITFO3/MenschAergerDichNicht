dd74859168fc7c2ba295a1c590e559ea6fccf9f2
DeathCookies <maedlerdaniel@gmail.com>
2015-09-08 05:54:10 -0300

    Neue Klassen

--- src/MenschAergerDichNicht/Spieler.java
+++ src/MenschAergerDichNicht/Spieler.java
@@ -1,10 +1,30 @@
 package MenschAergerDichNicht;
 
+import java.awt.Color;
+
 public class Spieler {
 
-	public Spieler() {
+	private Color spielfigurenFarbe;
+	private Figur[] eigeneSpielfiguren = new Figur[4];
+	
+	public Spieler(Color c) {
+		spielfigurenFarbe = c;
+		for (int i = 0; i < eigeneSpielfiguren.length; i++) {
+			eigeneSpielfiguren[i] = new Figur(c);
+		}
+		
+		
 		
 	}
 	
+	public int wuerfel() {
+		return Wuerfel.wuerfel();
+	}
 	
+	public static void main(String[] args) {
+		new Spieler(Color.red);
+	}
 }
+
+
+
---
+++ src/MenschAergerDichNicht/Wuerfel.java
@@ -0,0 +1,9 @@
+package MenschAergerDichNicht;
+
+public class Wuerfel {
+
+	public static int wuerfel() {
+		return (int)(Math.random() * (7.0 - 1.0) + 1.0);
+	}
+	
+}
