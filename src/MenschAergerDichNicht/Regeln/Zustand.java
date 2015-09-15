da5446b27a4b326ad3e46e2f5c49f029acc6671b
Dominik Bittner <DoBi-tyndur@gmx.net>
2015-09-15 05:19:09 -0300

    Regeln überarbeitet
    - Kommentare hinzugefügt
    - Abstrakte Klasse hinzugefügt
    - Zuständigkeitskette aufgebaut
    - UML-Diagramm überarbeitet

--- src/MenschAergerDichNicht/Regeln/Zustand.java
+++ src/MenschAergerDichNicht/Regeln/Zustand.java
@@ -1,14 +1,28 @@
 package MenschAergerDichNicht.Regeln;
 
+/**
+ * Zustandsklasse
+ * Diese wird an die Regeln uebergeben, um den aktuellen Zustand des Spielfeldes wiederzugeben
+ */
 public class Zustand {
 	private int wuerfelzahl;
 	
+	/**
+	 * Setter fuer die aktuell gewuerfelte Zahl
+	 *  
+	 * @param wuerfelzahl
+	 * @return Das aktuelle Zustand-Objekt
+	 */
 	public Zustand setWuerfelzahl(int wuerfelzahl) {
 		this.wuerfelzahl = wuerfelzahl;
 		
 		return this;
 	}
 
+	/**
+	 * Getter fuer die aktuell gewuerfelte Zahl
+	 * @return
+	 */
 	public int getWuerfelzahl() {
 		return this.wuerfelzahl;
 	}
