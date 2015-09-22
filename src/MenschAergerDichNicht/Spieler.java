03340a55e5dfc6bab82b35d9615184411ffa8fdf
Dominik Bittner <DoBi-tyndur@gmx.net>
2015-09-22 05:14:31 -0300

    Models überarbeitet + Graphik hinzugefügt

--- src/MenschAergerDichNicht/Spieler.java
+++ src/MenschAergerDichNicht/Spieler.java
@@ -1,26 +1,59 @@
 package MenschAergerDichNicht;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Spieler {
-
-	private Color spielfigurenFarbe;
-	private Figur[] eigeneSpielfiguren = new Figur[4];
+	private Color farbe;
+	private Figur[] figuren;
 	private String name;
 	
+	/**
+	 * Erstellt einen neuen Spieler inklusive der Spielfiguren
+	 * 
+	 * @param name  Der Spielername
+	 * @param c 	Die Farbe des Spielers
+	 */
 	public Spieler(String name, Color c) {
-		spielfigurenFarbe = c;
+		this.farbe = c;
 		this.name = name;
-		for (int i = 0; i < eigeneSpielfiguren.length; i++) {
-			eigeneSpielfiguren[i] = new Figur(name, c);
-		}		
+		this.figuren = new Figur[4];
 		
+		for (int i = 0; i < figuren.length; i++) {
+			figuren[i] = new Figur(name, c);
+		}
 	}
 	
-	public int wuerfel() {
-		return Wuerfel.wuerfel();
+	/**
+	 * Gibt die Spielerfarbe zurueck
+	 * 
+	 * @return Color
+	 */
+	public Color getFarbe() {
+		return farbe;
+	}
+
+	/**
+	 *  Gibt den Spielernamen zurueck
+	 * @return String
+	 */
+	public String getName() {
+		return name;
 	}
 	
+	/**
+	 * Gibt die Figuren des Spielers als ArrayList zurueck
+	 * @return ArrayList<Figur>
+	 */
+	public ArrayList<Figur> getFiguren() {
+		ArrayList<Figur> figuren = new ArrayList<Figur>();
+		
+		for (int i = 0; i < this.figuren.length; i++) {
+			figuren.add(this.figuren[i]);
+		}
+		
+		return figuren;
+	}
 }
 
 
