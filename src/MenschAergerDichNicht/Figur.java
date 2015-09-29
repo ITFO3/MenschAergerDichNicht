f6f68d6b4ace974a03ed55f88ea8af9becf442e2
Julian Brands <nox.nox.core@gmail.com>
2015-09-29 06:03:21 -0300

    Controller

--- src/MenschAergerDichNicht/Figur.java
+++ src/MenschAergerDichNicht/Figur.java
@@ -12,19 +12,28 @@ public class Figur extends Observable {
 	private Color eigeneFarbe;
 	private String id;
 	private int position;
+	private int hausFeld;
+	private Spieler s;
 
 	
 	static private int tempCounter = 1;
 	
 	
 	
-	public Figur(String name, Color c) {
+	public Figur(String name, Color c, Spieler s, int hausFeld) {
 		
 		position = 0;
 		eigeneFarbe = c;
 		id = name + ":" + tempCounter++;
+		this.s = s;
+		this.hausFeld = hausFeld;
+		this.setPosition(hausFeld);
 	}
 	
+	public int getHausFeld() {
+		return hausFeld;
+	}
+
 	public Color getColor() {
 		return this.eigeneFarbe;
 	}
@@ -35,6 +44,11 @@ public class Figur extends Observable {
 	
 	public int getPosition() {
 		return position;
+	}
+	
+	public int getStartFeld() {
+		
+		return s.gibStartFeld();
 	}
 	
 	public void setPosition(int newPosition) {
--- src/MenschAergerDichNicht/Wuerfel.java
+++ src/MenschAergerDichNicht/Wuerfel.java
@@ -9,7 +9,12 @@ public class Wuerfel {
 	 * @return Eine 'Zufallszahl' ziwschen 1 und 6
 	 */
 	public static int wuerfel() {
-		return (int)(Math.random() * (7.0 - 1.0) + 1.0);
+		return (int)(Math.random() * 6.0 + 1.0);
 	}
+	
+	public static int ermittleBeginner(int spielerAnzahl) {
+		return (int)(Math.random() * spielerAnzahl + 1.0);
+	}
+	
 	
 }
