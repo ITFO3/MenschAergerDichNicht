ee609b7a0d6adc820d02860ec7be28dfbd9d045f
Dominik Bittner <DoBi-tyndur@gmx.net>
2015-11-23 20:27:08 +0100

    Tests zurückgesetzt
    Signed-off-by: Dominik Bittner <DoBi-tyndur@gmx.net>

--- src/view/TestRun.java
+++ src/view/TestRun.java
@@ -30,40 +30,40 @@ public class TestRun {
 		
 		ClientController controller = new ClientController();
 		
-		Figur figur2 = new Figur("Test2", Color.blue, 10, 0, 0);
-		figur2.addObserver(model);
+		//Figur figur2 = new Figur("Test2", Color.blue, 10);
+		//figur2.addObserver(model);
 		
 		// Positionen verschieben
-		int j = 10;
-		for (int i = 0; i <= 40; i++) {
-			try {
-				controller.aktualisiereSpielfeld(figurId, null, i);
-				figur2.setPosition(j);
-				j++;
-				
-				if (j > 40) {
-					j = 1;
-				}
-				
-				Thread.sleep(1000);
-			} catch(InterruptedException ex) {
-				Thread.currentThread().interrupt();
-			}
-		}
+//		int j = 10;
+//		for (int i = 0; i <= 40; i++) {
+//			try {
+//				controller.aktualisiereSpielfeld(figurId, null, i);
+////				figur2.setPosition(j);
+//				j++;
+//				
+//				if (j > 40) {
+//					j = 1;
+//				}
+//				
+//				Thread.sleep(1000);
+//			} catch(InterruptedException ex) {
+//				Thread.currentThread().interrupt();
+//			}
+//		}
 		
-//		Figur figur2 = new Figur("Test2", Color.BLUE, -2 , 0, 39);
-//		figur2.addObserver(model);
-//		figur2.setPosition(-2);
-//		Figur figur3 = new Figur("Test3", Color.YELLOW, -3 , 0, 39);
-//		figur3.addObserver(model);
-//		figur3.setPosition(-3);
-//		
-//		ArrayList<String> moeglichkeiten = new ArrayList<String>();
-//		moeglichkeiten.add(figurId);
-//		moeglichkeiten.add(figur2.getId());
-//		moeglichkeiten.add(figur3.getId());
-//		
-//		controller.zeigeMoeglichkeiten(moeglichkeiten, 4);
+		Figur figur2 = new Figur("Test2", Color.BLUE, -2 , 0, 39);
+		figur2.addObserver(model);
+		figur2.setPosition(-2);
+		Figur figur3 = new Figur("Test3", Color.YELLOW, -3 , 0, 39);
+		figur3.addObserver(model);
+		figur3.setPosition(-3);
+		
+		ArrayList<String> moeglichkeiten = new ArrayList<String>();
+		moeglichkeiten.add(figurId);
+		moeglichkeiten.add(figur2.getId());
+		moeglichkeiten.add(figur3.getId());
+		
+		controller.zeigeMoeglichkeiten(moeglichkeiten, 4);
 	}
 
 }
